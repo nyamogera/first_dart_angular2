d6a8797c09e83b06a46f5708a51729a42d5a2e45
nyamogera <nyamogera@gmail.com>
2016-03-06 00:48:58 +0900

    エディター部分が動いた

---
+++ web/lib/ace.dart
@@ -0,0 +1,7 @@
+import "package:js/js.dart";
+
+@anonymous
+@JS()
+class ace {
+  external static void edit(String id);
+}
--- web/main.dart
+++ web/main.dart
@@ -2,12 +2,13 @@ import 'package:angular2/angular2.dart';
 import 'package:angular2/bootstrap.dart';
 import 'dock_spawn/dock_spawn.dart';
 import 'dart:html';
+import "lib/ace.dart";
 
 @Component(
     selector: 'my-app',
     template:
     '<div id="my_dock_manager" class="my-dock-manager">'
-        '<div id="editor_window" caption="Editor"><div style="background-color:yellow">エディター</div></div>'
+        '<div id="editor_window" caption="Editor"><div id="editor" style="width: 100%;height: 100%;">エディター</div></div>'
         '<div id="viewer_window" caption="Viewer"><div style="background-color:blue">ビューワー</div></div>'
         '<div id="console_window" caption="console"><div style="background-color:red">コンソール</div></div>'
         '</div>'
@@ -15,6 +16,7 @@ import 'dart:html';
 class AppComponent implements AfterViewInit {
 
   DockManager dockManager;
+
 
   AppComponent() {
     print("constractor!!");
@@ -40,6 +42,9 @@ class AppComponent implements AfterViewInit {
     DockNode consoleNode = dockManager.dockDown(documentNode, console, 0.2);
     DockNode viwerNode = dockManager.dockRight(documentNode, viwer, 0.5);
     DockNode editorNode = dockManager.dockFill(documentNode, editor);
+
+    ace.edit("editor");
+    print("callEditor");
   }
 
   void onResized(Event event) {
